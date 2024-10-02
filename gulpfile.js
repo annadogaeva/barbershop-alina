03d7dc7a14d1cef20be18c641f270a355dfd3665
alina-adell <lyhaalina@gmail.com>
2024-10-02 12:42:25 +0200

    021002

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const { src, dest, watch, series, } = require("gulp");
+const {src, dest, watch, series,} = require("gulp");
 const sass = require('gulp-sass')(require('sass'));
-const cssmin = require('gulp-cssmin');
+const cssmin = require('gulp-concat');
 const rename = require('gulp-rename');
 const browserSync = require("browser-sync");
 const sourcemaps = require("gulp-sourcemaps");
@@ -13,13 +13,13 @@ function SASS() {
     return src('./styles/*.scss')  // берём все SASS-файлы
         .pipe(sourcemaps.init())
         .pipe(sass().on("error", notify.onError()))
-        .pipe(cssmin())
-        .pipe(rename({suffix: '.min'}))
+        .pipe(cssmin('bundle.min.css'))
         .pipe(sourcemaps.write("."))
         .pipe(dest('dist/styles/')) //// выгружаем результат
         .pipe(browserSync.stream());
 
 }
+
 exports.SASS = SASS;
 
 function html() {
@@ -30,7 +30,7 @@ function html() {
 
 exports.html = html;
 
- function fonts() {
+function fonts() {
     return src('./fonts/*')
         .pipe(dest('dist/fonts'))
         .pipe(browserSync.stream());
@@ -38,18 +38,20 @@ exports.html = html;
 
 exports.fonts = fonts;
 
-function images () {
+function images() {
     return src('./images/*')
         .pipe(dest('dist/images'))
         .pipe(browserSync.stream());
 }
+
 exports.images = images;
 
-function scripts () {
+function scripts() {
     return src('./scripts/*')
         .pipe(dest('dist/scripts'))
         .pipe(browserSync.stream());
 }
+
 exports.scripts = scripts;
 
 function myServer() {
@@ -60,11 +62,11 @@ function myServer() {
         notify: false
     });
 
-    watch('./styles/*.scss', { usePolling: true }, SASS) // следим за SASS
+    watch('./styles/*.scss', {usePolling: true}, SASS) // следим за SASS
     watch('./*.html', series('html'))
     watch('./fonts/*', series('fonts'))
     watch('./images/*', series('images'))
     watch('./scripts/*', series('scripts'))
 }
 
-exports.default = series( SASS, html, fonts, images, scripts, myServer );
+exports.default = series(SASS, html, fonts, images, scripts, myServer);
